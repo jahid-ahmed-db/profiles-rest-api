23b87168ec98c19dcafa9723bcfec87b5351f0c3
jahmed21 <jahid.ahmed@hivehome.com>
2020-08-26 13:13:50 +0100

    Added POST, PUT, PATCH and DELETE

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,8 +1,15 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response 
+from rest_framework import status
+
+from profiles_api import serializer
+
 
 class HelloApiView(APIView):
     """Test API View"""
+    serializer_class = serializer.HelloSerializer
+
+
 
     def get(self, request, format=None):
         """Returns a list of APIviews features"""
@@ -16,3 +23,33 @@ class HelloApiView(APIView):
 
         return Response({'message': 'Hello', 'an_apiview': an_apiview })
 
+    def post(self, request): # request is example of handler which will hold client post request data
+        """Create a hello message with our name"""
+        serializer = self.serializer_class(data=request.data) # this invoke the class "Hello serializer" in serializer.py
+
+        if serializer.is_valid(): #validate client post request data meet serializer max_length
+            name  = serializer.validated_data.get('name') # get () is a method that returns the value of the item with the specified key.
+            message = f'Hello {name}'
+            return Response({'message': message})
+
+        else:
+            return Response(
+                serializer.errors, 
+                status=status.HTTP_400_BAD_REQUEST
+                )
+
+  
+    def put(self, request, pk=None):
+        """Handle updating an object"""
+    
+        return Response({'method': 'PUT'})
+    
+    def patch(self, request, pk=None):
+        """Handle partial update of object"""
+    
+        return Response({'method': 'PATCH'})
+    
+    def delete(self, request, pk=None):
+        """Delete an object"""
+    
+        return Response({'method': 'DELETE'})
