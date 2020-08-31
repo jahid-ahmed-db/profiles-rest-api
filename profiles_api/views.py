23c3f8cb5f2f5ef32fe7e396f461995c30a20cea
jahmed21 <jahid.ahmed@hivehome.com>
2020-08-31 17:43:39 +0100

    Added searching to user profile api

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -3,6 +3,8 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
+
 
 from profiles_api import serializers
 from profiles_api import models
@@ -108,7 +110,9 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     """Handle creating, creating and updating profiles"""
     serializer_class = serializers.UserProfileSerializer
     queryset = models.UserProfile.objects.all()
-    authentication_class = (TokenAuthentication,)
+    authentication_classes = (TokenAuthentication,)
     permissions_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)
 
 
