85e267f9a2c4ff5c9c8b34eb24fae21d6de30780
jahmed21 <jahid.ahmed@hivehome.com>
2020-09-08 14:07:01 +0100

    finalizing deployment script

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Profiles REST API
 
 Profiles REST API course code.
+
+
+Requirement: chmod +x deploy/*.sh
--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -23,7 +23,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '_#ycgvsr38#yczq8-tuv7r&u)-s_erfesuj%58d0zb56u$)-ma'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 1)))
+
+
+
 
 ALLOWED_HOSTS = []
 
@@ -123,3 +126,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'profiles_api.UserProfile'
+
+
+STATIC_ROOT = 'static/'
