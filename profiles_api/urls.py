29ca041d47c2e1bdd4df1936d20b8f43e1cf9379
jahmed21 <jahid.ahmed@hivehome.com>
2020-09-08 09:46:44 +0100

    :bulb: Added Login API

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -3,16 +3,18 @@ from rest_framework.routers import DefaultRouter
 from profiles_api import views
 # https://www.django-rest-framework.org/api-guide/routers/
 
-router = DefaultRouter()
-router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')
-router.register('profile', views.UserProfileViewSet)
+
 # register our url sub domain and this links to the class "HelloViewset" which will
 # handle the client request 
 
 
+router = DefaultRouter()
+router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')
 
+router.register('profile', views.UserProfileViewSet)
 
 urlpatterns = [
     path('hello-view/', views.HelloApiView.as_view()),
+    path('login/', views.UserLoginApiView.as_view()),
     path('', include(router.urls)),
 ]
